722e510c4e446e8aad974d84581e11d1ec9ba150
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2023-12-30 19:42:29 +0600

    better dc logging

--- utils/discord.py
+++ utils/discord.py
@@ -4,7 +4,7 @@ import json
 # Replace 'YOUR_WEBHOOK_URL' with the actual URL of your Discord webhook
 DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1108425291552010251/K5z6vd1Z9XG5HDgCWMyEB4_-H-E1-g3iikzC_rV7UmSnFqD-xcDZCmecOTjg_GcJctr0'
 
-def send_embedded_message(description: str, statsMessage: str, mentionTeam: bool = False):
+def send_embedded_message(description: str, statsMessage: str|dict, mentionTeam: bool = False):
   try:
 
     headers = {
@@ -12,14 +12,26 @@ def send_embedded_message(description: str, statsMessage: str, mentionTeam: bool
     }
 
     # Example usage with an embed
-    embed = {
-        'title': 'THESIS TRAINING NOTIFICATION',
-        'description': description,
-        'color': 1127128,
-        'fields': [
-            {'name': 'Stats', 'value': statsMessage, 'inline': True},
-        ]
-    }
+    if isinstance(statsMessage, str):
+
+        embed = {
+            'title': 'THESIS TRAINING NOTIFICATION',
+            'description': description,
+            'color': 1127128,
+            'fields': [
+                {'name': 'Stats', 'value': statsMessage, 'inline': True},
+            ]
+        }
+    else:
+        # value is a dictionary
+        embed = {
+            'title': 'THESIS TRAINING NOTIFICATION',
+            'description': description,
+            'color': 1127128,
+            'fields': [
+                {'name': key, 'value': str(value), 'inline': True} for key, value in statsMessage.items()
+            ]
+        }
 
 
     payload = {
@@ -38,4 +50,3 @@ def send_embedded_message(description: str, statsMessage: str, mentionTeam: bool
         print(f"Failed to send embedded message. Status code: {response.status_code}")
   except:
     pass
-
